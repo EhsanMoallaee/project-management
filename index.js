3de40d52ac8d4592c628227b05e387d3ec4f4c05
Ehsan Moallaee <ehsanm78@gmail.com>
2023-07-23 20:00:18 +0330

    Some modifications done and hard codes removed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Application = require('./app/server');
 require('dotenv').config();
 
-const DB_ULR = 'mongodb://127.0.0.1:27017/botostart-project-management';
-new Application(3000, DB_ULR)
+const DB_ULR = process.env.DB_URI;
+const PORT = process.env.PORT || 3000;
+
+new Application(PORT, DB_ULR)
